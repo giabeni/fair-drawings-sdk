1e609ddb84c2136184fcb96163ac909d7f2c17c6
Giovanni Abeni <giovanni@espressolabs.com.br>
2020-08-19 22:31:05 -0300

    set candidate indexes as readonly

--- src/draw/entities/candidate.entity.ts
+++ src/draw/entities/candidate.entity.ts
@@ -8,6 +8,6 @@ export class Candidate<P = object> extends Stakeholder<P> {
    * Uniform distribuition - in a draw with candidates [c0, c1], c0.indexes = [0] and c1.indexes = [1]
    * Not-uniform distribuition - in a draw with candidates [c0, c0, c1], c0.indexes = [0, 1] and c1.indexes = [2]
    */
-  indexes?: number[];
+  public readonly indexes?: number[];
 
 }
