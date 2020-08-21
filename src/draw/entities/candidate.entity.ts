2e12b33dd4665bcc0e2f629dba9c57063873172c
Giovanni Abeni <giovanni@espressolabs.com.br>
2020-08-20 23:46:13 -0300

    Added method isAtIndex to candidate

--- src/draw/entities/candidate.entity.ts
+++ src/draw/entities/candidate.entity.ts
@@ -8,4 +8,12 @@ export class Candidate<P = object> extends Stakeholder<P> {
    * Not-uniform distribuition - in a draw with candidates [c0, c0, c1], c0.indexes = [0, 1] and c1.indexes = [2]
    */
   public readonly indexes?: number[];
+
+  /**
+   * Tests if a candidate occupies a specific position (index) in a draw
+   * @param index the position number to test
+   */
+  public isAtIndex(index: number) {
+    return this.indexes?.includes(index);
+  }
 }
